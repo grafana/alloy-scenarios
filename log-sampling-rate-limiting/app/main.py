c512b1498c8efc0fd2fb2e16db4fc4224d95cfd7
Ksbhargav-creator <ksbhargav018@gmail.com>
2026-07-22 08:27:23 +0530

    Add demo script

--- log-sampling-rate-limiting/app/main.py
+++ log-sampling-rate-limiting/app/main.py
@@ -1,3 +1,7 @@
+import random
+import datetime
+import time
+import json
 # Data for the script
 
 DEBUG = [
@@ -20,15 +24,26 @@ ERROR = [
     "unhanded exception in handler"
 ]
 
-# Random pick from the list
-# Pick a random element from the
-# DEBUG,INFO,WARN and ERROR lists.
+levels = {"DEBUG": DEBUG, "INFO": INFO, "WARN": WARN, "ERROR": ERROR}
 
-# Build a Python dict with the following
-# keys: level, message, timestamp.
-
-# Convert the dict into JSON string
+#Clean the file
+with open("/logs/app.log", "a") as f:
+    pass
 
 # loop the lists
+while True:
+    # Random pick from the list
+    # Pick a random element from the
+    # DEBUG,INFO,WARN and ERROR lists.
+    choice = random.choices(list(levels.keys()), weights = [60,20,10,10], k=1)[0]
+    message_choice = random.choice(levels[choice])
 
-# Write to file path
+    # Build a Python dict with the following
+    # keys: level, message, timestamp.
+    json_object = {"level": choice, "message": message_choice, "timestamp": datetime.datetime.now().isoformat()}
+    # Convert the dict into JSON string
+    json_string = json.dumps(json_object)
+    # Write to file path
+    with open("/logs/app.log", "a") as f:
+        f.write(json_string + "\n")
+    time.sleep(0.1)
